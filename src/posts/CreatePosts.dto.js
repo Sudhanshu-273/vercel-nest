731a8ceae238104f5374c55e6623f07d9e13a2af
Sudhanshu Chaubey <sudhanshuchaubey273@gmail.com>
2024-04-11 23:50:19 +0530

    moment corrected

--- src/posts/CreatePosts.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreatePostsDto = void 0;
-var CreatePostsDto = /** @class */ (function () {
-    function CreatePostsDto() {
-    }
-    return CreatePostsDto;
-}());
-exports.CreatePostsDto = CreatePostsDto;
--- src/posts/dtos/AddComment.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AddCommentDto = void 0;
-var AddCommentDto = /** @class */ (function () {
-    function AddCommentDto() {
-    }
-    return AddCommentDto;
-}());
-exports.AddCommentDto = AddCommentDto;
