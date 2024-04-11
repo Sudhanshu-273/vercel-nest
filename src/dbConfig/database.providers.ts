af50f7d0f34459238bfb6b892360dff728b18b48
Sudhanshu Chaubey <sudhanshuchaubey273@gmail.com>
2024-04-11 23:24:16 +0530

    changed port config

--- src/dbConfig/database.providers.ts
+++ src/dbConfig/database.providers.ts
@@ -1,29 +1,29 @@
-import {Sequelize} from "sequelize-typescript";
+import { Sequelize } from "sequelize-typescript";
 
 export const databaseConfig: object = {
-    production: {
-        username: 'freedb_sudhanshu',
-        password: 'DVx6VegV#!2Mrxa',
-        database: 'freedb_social',
-        host: 'sql.freedb.tech',
-        port: 3306,
-        dialect: 'mysql',
-        dialectModule: require('mysql2'),
-        define: {
-            schema: 'freedb_social'
-        }
-    },
-    development: {
-        username: 'root',
-        password: 'Chaubey@123',
-        database: 'social',
-        host: 'localhost',
-        port: 3306,
-        dialect: 'mysql',
-        define: {
-            schema: 'social'
-        }
-    }
+      production: {
+            username: 'freedb_sudhanshu',
+            password: 'DVx6VegV#!2Mrxa',
+            database: 'freedb_social',
+            host: 'sql.freedb.tech',
+            port: process.env.DB_PORT || 3306,
+            dialect: 'mysql',
+            dialectModule: require('mysql2'),
+            define: {
+                  schema: 'freedb_social'
+            }
+      },
+      development: {
+            username: 'root',
+            password: 'Chaubey@123',
+            database: 'social',
+            host: 'localhost',
+            port: 3306,
+            dialect: 'mysql',
+            define: {
+                  schema: 'social'
+            }
+      }
 }
 
 export const sequelize = new Sequelize(databaseConfig["production"])
