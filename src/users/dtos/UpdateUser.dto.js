afe48a87116afa077f4bf713f3c7542ee93c49b7
Sudhanshu Chaubey <sudhanshuchaubey273@gmail.com>
2024-04-14 13:33:40 +0530

    multiple-images

--- src/users/dtos/UpdateUser.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateUserDto = void 0;
-var UpdateUserDto = /** @class */ (function () {
-    function UpdateUserDto() {
-    }
-    return UpdateUserDto;
-}());
-exports.UpdateUserDto = UpdateUserDto;
